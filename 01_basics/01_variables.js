815bde14e3a733336000a1f719d0b39470229067
Himmath Kumar <111720812+Himmath056@users.noreply.github.com>
2024-09-02 11:42:30 +0000

    add 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144423
+let accountEmail = "himmath@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2  not allowed
+
+accountEmail ="hK@gmail.com"
+accountPassword = "244342"
+accountCity ="bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer to not use var
+because of global scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
